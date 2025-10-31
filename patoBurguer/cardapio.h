81ed566c95241c83f520cc1f5b3b04b8eb78c7a7
Maike Vitério <maikeviterio@hotmail.com>
2025-10-31 09:49:04 -0300

    atua

--- patoBurguer/cardapio.h
+++ patoBurguer/cardapio.h
@@ -1,33 +1,48 @@
-#ifndef CARDAPIO_H
-#define CARDAPIO_H
 #include <stdio.h>
 #include <string.h>
-#include "ingredientes.h"
+#include "pilha.h"
 
-
-typedef struct
-{
-    int ID;
-    char nome[50];
-    char *ingredientes[20];
-    int qntIngredientes;
-    double preco;
-
-} hamburguer;
-
- 
-void hamburguers (hamburguer cardapio[10]) {
-
-    cardapio[0] = (hamburguer){1, "Bit and Bacon", {i[0], i[2], i[3], i[1], i[0]}, 5, 18.90};
-    cardapio[1] = (hamburguer){2, "Duck Cheese", {i[0], i[4], i[5], i[3], i[0]}, 5, 15.90};
-    cardapio[2] = (hamburguer){3, "Quackteirao", {i[0], i[4], i[3], i[1], i[3], i[0]}, 6, 16.90};
-    cardapio[3] = (hamburguer){4, "Big Pato", {i[0], i[1], i[4], i[0], i[1], i[3], i[4], i[0]}, 8, 18.90};
-    cardapio[4] = (hamburguer){5, "Brabo Patissimo", {i[0], i[6], i[4], i[1], i[1], i[3], i[9], i[0]}, 8, 19.90};
-    cardapio[5] = (hamburguer){6, "Tasty Donald", {i[0], i[6], i[7], i[4], i[5], i[3], i[1], i[3], i[0]}, 9, 20.50};
-    cardapio[6] = (hamburguer){7, "Patolino Burguer", {i[0], i[10], i[3], i[4], i[6], i[7], i[8], i[3], i[0]}, 9, 20.00};
-    cardapio[7] = (hamburguer){8, "Cheddar Quack Melt", {i[0], i[1], i[3], i[3], i[7], i[0]}, 6, 17.50};
-    cardapio[8] = (hamburguer){9, "Patao", {i[0], i[1], i[1], i[3], i[6], i[2], i[11], i[0]}, 8, 22.00};
-    cardapio[9] = (hamburguer){10, "Veganito do Pato", {i[13], i[12], i[5], i[7], i[4], i[14], i[13]}, 7, 22.50};
+char ingredientes[15][20] = {
+"Pao", "Carne", "Bacon", "Queijo", "Alface", "Tomate",
+"Molho Especial", "Cebola", "Picles", "Cebola Crispy",
+"Frango", "Onion Rings", "Carne de soja", "Pao integral", "Abacate"
 };
 
-#endif
+typedef struct {
+int ID;
+char nome[50];
+char *ingredientes[20];
+int qntIngredientes;
+double preco;
+} hamburguer;
+
+hamburguer cardapio[10];
+
+void cria_cardapio() {
+cardapio[0] = (hamburguer){1, "Bit and Bacon", {ingredientes[0], ingredientes[2], ingredientes[3], ingredientes[1], ingredientes[0]}, 5, 18.90};
+cardapio[1] = (hamburguer){2, "Duck Cheese", {ingredientes[0], ingredientes[4], ingredientes[5], ingredientes[3], ingredientes[0]}, 5, 15.90};
+cardapio[2] = (hamburguer){3, "Quackteirao", {ingredientes[0], ingredientes[4], ingredientes[3], ingredientes[1], ingredientes[3], ingredientes[0]}, 6, 16.90};
+cardapio[3] = (hamburguer){4, "Big Pato", {ingredientes[0], ingredientes[1], ingredientes[4], ingredientes[0], ingredientes[1], ingredientes[3], ingredientes[4], ingredientes[0]}, 8, 18.90};
+cardapio[4] = (hamburguer){5, "Brabo Patissimo", {ingredientes[0], ingredientes[6], ingredientes[4], ingredientes[1], ingredientes[1], ingredientes[3], ingredientes[9], ingredientes[0]}, 8, 19.90};
+cardapio[5] = (hamburguer){6, "Tasty Donald", {ingredientes[0], ingredientes[6], ingredientes[7], ingredientes[4], ingredientes[5], ingredientes[3], ingredientes[1], ingredientes[3], ingredientes[0]}, 9, 20.50};
+cardapio[6] = (hamburguer){7, "Patolino Burguer", {ingredientes[0], ingredientes[10], ingredientes[3], ingredientes[4], ingredientes[6], ingredientes[7], ingredientes[8], ingredientes[3], ingredientes[0]}, 9, 20.00};
+cardapio[7] = (hamburguer){8, "Cheddar Quack Melt", {ingredientes[0], ingredientes[1], ingredientes[3], ingredientes[3], ingredientes[7], ingredientes[0]}, 6, 17.50};
+cardapio[8] = (hamburguer){9, "Patao", {ingredientes[0], ingredientes[1], ingredientes[1], ingredientes[3], ingredientes[6], ingredientes[2], ingredientes[11], ingredientes[0]}, 8, 22.00};
+cardapio[9] = (hamburguer){10, "Veganito do Pato", {ingredientes[13], ingredientes[12], ingredientes[5], ingredientes[7], ingredientes[4], ingredientes[14], ingredientes[13]}, 7, 22.50};
+}
+
+void monta_hamburguer(hamburguer h) {
+tp_pilha p;
+inicializa_pilha(&p);
+
+printf("--- %s ---\n", h.nome);
+
+for (int i = 0; i < h.qntIngredientes; i++) {
+push(&p, *h.ingredientes[i]);
+}
+
+
+for (int i = h.qntIngredientes - 1; i >= 0; i--) {
+printf("%s\n", h.ingredientes[i]);
+}
+}
---
+++ patoBurguer/pilha.h
@@ -0,0 +1,66 @@
+#ifndef PILHA_H
+#define PILHA_H
+
+#include <stdio.h>
+
+#define MAX 100
+
+// Tipo de item da pilha
+typedef int 
+    tp_itemm;
+
+// Estrutura da pilha
+typedef struct {
+    int topo;
+    tp_item item[MAX];
+} tp_pilha;
+
+// Inicializa a pilha
+void inicializa_pilha(tp_pilha *p) {
+    p->topo = -1;
+}
+
+// Verifica se a pilha está vazia
+int pilhaVazia(tp_pilha *p) {
+    return p->topo == -1;
+}
+
+// Verifica se a pilha está cheia
+int pilhaCheia(tp_pilha *p) {
+    return p->topo == MAX - 1;
+}
+
+// Empilha um elemento
+int push(tp_pilha *p, tp_item e) {
+    if (pilhaCheia(p)) {
+        return 0;  // Falha ao empilhar (pilha cheia)
+    }
+    p->topo++;
+    p->item[p->topo] = e;
+    return 1;  // Sucesso
+}
+
+// Desempilha um elemento
+int pop(tp_pilha *p, tp_item *e) {
+    if (pilhaVazia(p)) {
+        return 0;  // Falha ao desempilhar (pilha vazia)
+    }
+    *e = p->item[p->topo];
+    p->topo--;
+    return 1;  // Sucesso
+}
+
+int alturaPilha(tp_pilha *p) {
+    return p->topo+1;
+}
+
+void imprimePilha(tp_pilha p) {
+    tp_item e;
+    printf("\n");
+    while (!pilhaVazia(&p)) {
+        pop(&p, &e);
+        printf("%d", e);
+    }
+}
+
+#endif  // PILHA_H
